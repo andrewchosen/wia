3bcb7845c3efb57211a08026d3c2db2e3bd67033
andrewchosen <andrewchosen@fullsail.edu>
2015-08-07 18:08:13 -0700

    updated file structure

--- homework1/js/main.js
+++ homework1/js/main.js
@@ -68,7 +68,20 @@ Use the arc method
 
 //Draw Circle here
 
+	var theCanvas2 = document.getElementById("Canvas2");
+	if(theCanvas2){
+		var ctx2 = theCanvas2.getContext("2d");
 
+		if(ctx2){
+			ctx2.beginPath();
+      		ctx2.arc(50, 50, 20, 0, 2 * Math.PI);
+		    ctx2.fillStyle = 'rgba(255,0,0,.5)';
+      		ctx2.fill();
+      		ctx2.lineWidth = 5;
+      		ctx2.strokeStyle = 'black';
+      		ctx2.stroke();
+		}
+	}
 
 
 /*******************************************
@@ -85,6 +98,24 @@ Height and width and color are up to you.
 
 //Draw Star here
 
+	var theCanvas3 = document.getElementById("Canvas3");
+	if(theCanvas3){
+		var ctx3 = theCanvas3.getContext("2d");
+
+		if(ctx3){
+      		ctx3.fillStyle = '#335500';
+
+      		ctx3.beginPath();
+      		ctx3.moveTo(100,100);
+      		ctx3.lineTo(300,100);
+      		ctx3.lineTo(125,225);
+      		ctx3.lineTo(200,50);
+      		ctx3.lineTo(275,225);
+      		ctx3.closePath();
+      		ctx3.fill();
+		}
+	}
+
 
 /*******************************************
 PART 4
@@ -100,6 +131,27 @@ Do not overlap any other object.
 
 //Draw Umbrella top here
 
+	var theCanvas4 = document.getElementById("Canvas4");
+	if(theCanvas4){
+		var ctx4 = theCanvas4.getContext("2d");
+
+		if(ctx4){
+			ctx4.fillStyle = 'rgba(255,0,0,.5)';
+			ctx4.lineWidth = 5;
+      		ctx4.strokeStyle = 'black';
+			ctx4.beginPath();
+      		ctx4.arc(200, 200, 100, 0, 1 * Math.PI,true);
+      		ctx4.bezierCurveTo(100,150,150,150,150,200);
+      		ctx4.bezierCurveTo(150,150,200,150,200,200);
+      		ctx4.bezierCurveTo(200,150,250,150,250,200);
+      		ctx4.bezierCurveTo(250,150,300,150,300,200);
+      		ctx4.closePath();
+
+      		ctx4.fill();
+      		ctx4.stroke();
+		}
+	}
+
 /*******************************************
 PART 5
 
@@ -109,6 +161,26 @@ Draw text into your canvas.  It can said whatever you would like in any color.
 ********************************************/
 
 //Draw text here
+
+var theCanvas5 = document.getElementById("Canvas5");
+	if(theCanvas5){
+		var ctx5 = theCanvas5.getContext("2d");
+
+		if(ctx5){
+			var theString = "I'm drawing text, man!";
+
+			ctx5.fillStyle = "#e5e5e5";
+			ctx5.strokeStyle = "rgba(150,0,0,0.5)";
+			ctx5.textBaseline = "middle";
+			ctx5.font = "36pt Arial";
+			ctx5.fillText(theString, 60, 200);
+			ctx5.strokeText(theString, 63, 203);
+			
+
+      		ctx5.fill();
+      		ctx5.stroke();
+		}
+	}
 
 /*******************************************
 PART 6
@@ -125,6 +197,26 @@ Reminder to use the drawImage method for all 3 of the ways.
 
 //Draw images here
 
+	var theCanvas6 = document.getElementById("Canvas6");
+	if(theCanvas6){
+		var ctx6 = theCanvas6.getContext("2d");
+
+		if(ctx6){
+			var img = document.getElementById("img1");
+			img.onload = function(){
+				// Image 1
+				ctx6.drawImage(img,0,0);
+
+				// Image 2
+				ctx6.drawImage(img,0,1100,1650,544);
+
+				// Image 3
+				ctx6.drawImage(img,500,500,250,250,250,1700,250,250);
+
+
+			}
+		}
+	}
 
 
 /*******************************************
@@ -139,6 +231,41 @@ You must use at least 3 different methods.
 ********************************************/
 
 // Draw scene here
+
+	var theCanvas7 = document.getElementById("Canvas7");
+	if(theCanvas7){
+		var ctx7 = theCanvas7.getContext("2d");
+
+		if(ctx7){
+			ctx7.beginPath();
+      		ctx7.arc(350, 350, 250, 0, 2 * Math.PI);
+		    ctx7.fillStyle = 'rgba(255,255,0,.5)';
+      		ctx7.fill();
+      		ctx7.lineWidth = 5;
+      		ctx7.strokeStyle = 'black';
+      		ctx7.stroke();
+
+      		ctx7.beginPath();
+      		ctx7.fillStyle = "black";
+      		ctx7.arc(250,300,40,0,2 * Math.PI);
+      		ctx7.arc(450,300,40,0,2 * Math.PI);
+      		ctx7.fill();
+
+      		ctx7.beginPath();
+      		ctx7.moveTo(250,450);
+      		ctx7.lineTo(450,450);
+      		ctx7.stroke();
+
+      		var img2 = document.getElementById("bandaid");
+
+      		img2.onload = function(){
+      			ctx7.drawImage(img2,300,130,160,120)
+      		}
+
+		}
+	}
+
+// If no Modernizr, do this
 
 } else {
 	// Canvas is not supported
