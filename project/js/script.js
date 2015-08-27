65c7963f0f0090aadc8cb6ae59fc26a27a770513
andrewchosen <andrewchosen@fullsail.edu>
2015-08-26 23:55:40 -0700

    Week 4 updates

--- project/js/script.js
+++ project/js/script.js
@@ -2,16 +2,14 @@
 // on 8/18/2015
 
 $(document).ready(function(){
+	$("span").animate({"width": "100px"}, 1000);
 
-	$("ul").hoverIntent({
-	    over: slideDown,
-	    out: slideUp,
-	    selector: 'li',
-	    timeout: 150,
-	    sensitivity: 3
+	$(".tab").click(function(){
+		$(".tab").removeClass("active");
+		$(this).addClass("active");
+		$("section#about").find("article").slideUp(500, "jswing");
+		var tabId = $(this).attr("data-id");
+		$(tabId).slideDown(1000, "jswing");
+
 	});
-
 });
-
-function slideDown(){$(this).find("ul").slideDown(200);}
-function slideUp(){$(this).find("ul").slideUp(200);}
